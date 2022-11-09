6b5d3630c7aa8aff7fc4cf5f33f3052e210fc8a0
Anubhaaav <anubhavbarooah9@gmail.com>
2022-11-09 23:30:09 +0530

    updating css but couldn't solve js

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
 
-    let user = prompt("Enter R,P or S");
-    let cpu1 = Math.floor(Math.random() * 3);
-    let cpu = ["R", "P", "S"][cpu1];
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        makeSelection(selectionName);
+    })
+})
 
-    const match = (cpu, user) =>{
-        if(cpu === user){
-            return "Nobody: Match is tied";
-        }
-        else if(cpu == "R" && user =="P"){
-            return "user"
-        }
-        else if(cpu == "S" && user =="P"){
-            return "cpu"
-        }
-        else if(cpu == "R" && user =="S"){
-            return "cpu"
-        }
-        else if(cpu == "P" && user =="S"){
-            return "user"
-        }
-        else if(cpu == "P" && user =="R"){
-            return "cpu"
-        }
-        else if(cpu == "S" && user =="R"){
-            return "user"
-        }
-    }
-
-let result = match(cpu, user)
-document.write(`CPU:${cpu} <br> and User: ${user} <br> The winner is :${result} <br>`)
+function makeSelection(selection) {
+    console.log(selection);
+}
