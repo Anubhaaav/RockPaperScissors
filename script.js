138c198df697442421dd039036d45f9847bfaf33
Anubhaaav <anubhavbarooah9@gmail.com>
2022-11-12 20:57:02 +0530

    starting again

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const selectionButtons = document.querySelectorAll('[data-selection]');
+const selectionButtons = document.querySelectorAll('[data-selection]')
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection;
-        makeSelection(selectionName);
+        const selectionName = selectionButton.dataset.selection
+        makeSelection(selectionName)
     })
 })
 
